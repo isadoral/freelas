9ca24e51287ceceeb8f722f39fc9e09398b003fb
isadoral <isadora.lopes@code.berlin>
2024-11-10 07:05:24 +0100

    feat(api): create company account

---
+++ auth/src/routes/RegisterCompany.ts
@@ -0,0 +1,64 @@
+import express, { Request, Response } from "express";
+import { body } from "express-validator";
+import jwt from "jsonwebtoken"
+import { validateRequest, BadRequestError } from "@izzietx/common"
+
+import { Company } from "../models/Company";
+
+const router = express.Router();
+
+router.post("/api/users/registercompany",
+    [
+
+        body("email")
+            .isEmail()
+            .withMessage("Email must be valid"),
+        body("password")
+            .trim()
+            .isLength({ min: 4, max: 20 })
+            .withMessage("Password must be between 4 and 20 characters"),
+    ],
+    validateRequest,
+    async (req: Request, res: Response) => {
+        const { email, password, name, address, countries, description, billingEmail, hiringManagers, logo, extra, website } = req.body;
+
+        const existingCompany = await Company.findOne({ email });
+
+        if (existingCompany) {
+            throw new BadRequestError("Email already in use");
+        }
+
+        const user = Company.build({
+            address: { city: address.city, country: address.country, number: address.number, street: address.street, zipcode: address.zipcode },
+            billingEmail: billingEmail,
+            countries: countries,
+            description: description,
+            email: email,
+            emailConfirmed: false,
+            extra: extra,
+            hiringManagers: hiringManagers,
+            logo: logo,
+            name: name,
+            password: password,
+            userType: "company",
+            website: website
+        })
+        await user.save();
+
+        // Generate JWT
+        const userJwt = jwt.sign({
+                id: user.id,
+                email: user.email
+            },
+            process.env.JWT_KEY!
+        );
+
+        // Store it on session object
+        req.session = {
+            jwt: userJwt
+        };
+
+        res.status(201).send(user);
+    });
+
+export { router as registerCompanyRouter };
