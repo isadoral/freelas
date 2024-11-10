f9005dcb01bee7511111634b2ecf9b51e86076e1
isadoral <isadora.lopes@code.berlin>
2024-11-10 07:07:45 +0100

    feat(api): login to freela account

---
+++ auth/src/routes/LoginFreela.ts
@@ -0,0 +1,56 @@
+import express, { Request, Response } from "express";
+import { body } from "express-validator";
+import jwt from "jsonwebtoken";
+import { validateRequest, BadRequestError } from "@izzietx/common";
+
+import { Password } from "../services/password";
+import { Freela } from "../models/Freela";
+
+const router = express.Router();
+
+router.post("/api/users/loginfreela",
+    [
+        body("email")
+            .isEmail()
+            .withMessage("Email must be valid"),
+        body("password")
+            .trim()
+            .notEmpty()
+            .withMessage("You must supply a password ")
+    ],
+    validateRequest,
+    async (req: Request, res: Response) => {
+        const { email, password } = req.body;
+
+        const existingFreela = await Freela.findOne({ email });
+        if (!existingFreela) {
+            throw new BadRequestError("Invalid Credentials");
+        }
+
+        const passwordsMatch = await Password.compare(
+            existingFreela.password,
+            password
+        );
+
+        if (!passwordsMatch) {
+            throw new BadRequestError("Invalid Credentials");
+        }
+
+        // Generate JWT
+        const userJwt = jwt.sign({
+                id: existingFreela.id,
+                email: existingFreela.email
+            },
+            process.env.JWT_KEY!
+        );
+
+        // Store it on session object
+        req.session = {
+            jwt: userJwt
+        };
+
+        res.status(200).send(userJwt);
+
+    });
+
+export { router as loginFreelaRouter };
